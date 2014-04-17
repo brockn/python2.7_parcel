b654cb55be97beaac83a0053dc345e41b2f90271
Brock Noland <brock@cloudera.com>
2014-04-17 10:14:56 -0400

    Building parcel and all libraries works

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # exit on any error
 set -e
+set -x
 # where to find cm tools
 CM_EXT=${CM_EXT:-/opt/local/cm_ext/}
 # http accessible dir to install parcels
 PARCEL_REPO=${PARCEL_REPO:-/var/www/html/parcels/}
 SRC_DIR=$PWD
 BUILD_DIR=${BUILD_DIR:-$SRC_DIR/target}
-DOWNLOAD_DIR=${BUILD_DIR:-$SRC_DIR/downloads}
+DOWNLOAD_DIR=${DOWNLOAD_DIR:-$SRC_DIR/downloads}
 # where the parcel should be installed on the *slaves*
 INSTALL_DIR=${INSTALL_DIR:-/opt/cloudera/parcels/python2.7/}
 TABLEAU_PACKAGE=${TABLEAU_PACKAGE:-$DOWNLOAD_DIR/TDE-API-Python-Linux-64Bit.gz}
@@ -15,32 +16,14 @@ TABLEAU_PACKAGE=${TABLEAU_PACKAGE:-$DOWNLOAD_DIR/TDE-API-Python-Linux-64Bit.gz}
 OS=el6
 NATIVE_DEPS=(gcc zlib-devel readline-devel gcc-c++ openssl-devel tcl-devel tk-devel \
   gdbm-devel db4-devel blas-devel lapack-devel libpng-devel)
-PYTHON_DEP_NAMES=(numpy dateutil pandas scipy matplotlib pyflakes statsmodel ipython \
+PYTHON_DEP_NAMES=(numpy dateutil pandas scipy matplotlib pyflakes statsmodels \
   dataextract)
-if [[ -d $INSTALL_DIR ]]
-then
-  echo "Intall dir $INSTALL_DIR already exits, exiting"
-  exit 1
-fi
-sudo rm -rf $INSTALL_DIR
-sudo mkdir $INSTALL_DIR
-sudo chown $USER $INSTALL_DIR
-if [[ ! -f $CM_EXT/validator/target/validator.jar ]]
-then
-  echo "CM Ext $CM_EXT does not exist, exiting"
-  exit 1
-fi
-if [[ ! -d $PARCEL_REPO ]]
-then
-  echo "Parcel Repo $PARCEL_REPO does not exist, exiting"
-  exit 1
-fi
 # version, note that we may version the 2.7.5 python install ourselves (enable/disable modules)
 version=2.7.5.$(date +%Y%m%d.%H%M)
 # name of pracel
-name=python2.7-$version
+parcelName=python2.7-$version
 # dir we will tar to make the parcel
-stagingDir=$BUILD_DIR/$name
+stagingDir=$BUILD_DIR/$parcelName
 
 download_package() {
   if [[ -z "$1" ]]
@@ -48,8 +31,8 @@ download_package() {
     echo "ERROR: Invalid package '$1'"
     exit 1
   fi
-  name=$(basename $1)
-  if [[ ! -f $BUILD_DIR/$name ]]
+  local name=$(basename $1)
+  if [[ ! -f $DOWNLOAD_DIR/$name ]]
   then
     pushd $DOWNLOAD_DIR
     curl -O "$1"
@@ -61,11 +44,11 @@ download_package() {
     popd
   fi
   pushd $BUILD_DIR
-  tar -zxf $DOWNLAOD_DIR/$name
+  tar -zxf $DOWNLOAD_DIR/$name
   popd
 }
 build_package() {
-  if [[ -z "$1" ]] || [[ ! -d "$1" ]]
+  if [[ -z "$1" ]] || [[ ! -d "$BUILD_DIR/$1" ]]
   then
     echo "ERROR: Invalid package directory '$1'"
     exit 1
@@ -77,7 +60,7 @@ build_package() {
 }
 
 download_build() {
-  if [[ -z "$1" ]] !| [[ -z "$2"  ]]
+  if [[ -z "$1" ]] || [[ -z "$2"  ]]
   then
     echo "ERROR: Expected two args, but got '$1' and '$2'"
     exit 1
@@ -85,8 +68,26 @@ download_build() {
   download_package "$2"
   build_package "$1"
 }
+# pre-build checks
+if [[ -d $INSTALL_DIR ]]
+then
+  echo "Intall dir $INSTALL_DIR already exits, exiting"
+  exit 1
+fi
+sudo mkdir $INSTALL_DIR
+sudo chown $USER $INSTALL_DIR
+if [[ ! -f $CM_EXT/validator/target/validator.jar ]]
+then
+  echo "CM Ext $CM_EXT does not exist, exiting"
+  exit 1
+fi
+if [[ ! -w $PARCEL_REPO ]]
+then
+  echo "Parcel Repo $PARCEL_REPO either does not exist or is not writable, exiting"
+  exit 1
+fi
 
-if [[ -f $TABLEAU_PACKAGE ]]
+if [[ ! -f $TABLEAU_PACKAGE ]]
 then
   echo "ERROR: Expected Tableau TDE Python Package at $TABLEAU_PACKAGE"
   exit 1
@@ -109,17 +110,24 @@ do
   fi
 done
 
-rm -rf $BUILD_DIR
+sudo rm -rf $BUILD_DIR
 mkdir -p $stagingDir
+
+#####################
+## Parcel build steps
+#####################
 # copy meta to staging dir
 cp -R meta $stagingDir
 # update the parcel.json with the correct version/os
 perl -i -pe "s@%VERSION%@$version@g" $stagingDir/meta/parcel.json
 perl -i -pe "s@%OS%@$OS@g" $stagingDir/meta/parcel.json
+
+#####################
+## Python build steps
+#####################
 cd $BUILD_DIR
-# download pythong 2.7.5
-curl -O https://www.python.org/ftp/python/2.7/Python-2.7.tgz
-tar -zxf Python-2.7.tgz
+# download python 2.7.5
+download_package https://www.python.org/ftp/python/2.7/Python-2.7.tgz
 pushd Python-2.7
 patch -p1 < $SRC_DIR/src/main/patch/Python-2.7.patch
 # compile
@@ -129,9 +137,9 @@ make install
 
 # steps ordered based on dependencies
 download_build "numpy-1.8.1" "http://iweb.dl.sourceforge.net/project/numpy/NumPy/1.8.1/numpy-1.8.1.tar.gz"
-download_build "python-dateutil-1.5" "https://labix.org/download/python-dateutil/python-dateutil-1.5.tar.gz"
-# setuptools TODO is this required here?
+# setuptools
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | $INSTALL_DIR/bin/python
+download_build "python-dateutil-1.5" "https://labix.org/download/python-dateutil/python-dateutil-1.5.tar.gz"
 download_build "pandas-0.13.0" "https://pypi.python.org/packages/source/p/pandas/pandas-0.13.0.tar.gz"
 download_build "scipy-0.13.3" "http://iweb.dl.sourceforge.net/project/scipy/scipy/0.13.3/scipy-0.13.3.tar.gz"
 download_build "matplotlib-1.3.1" "http://softlayer-dal.dl.sourceforge.net/project/matplotlib/matplotlib/matplotlib-1.3.1/matplotlib-1.3.1.tar.gz"
@@ -167,18 +175,19 @@ do
   fi
 done
 
-
+#####################
+## Parcel build steps
+#####################
 # move the compiled contents to staing dir
 mv $INSTALL_DIR/* $stagingDir/
-rmdir $INSTALL_DIR
+sudo rmdir $INSTALL_DIR
 # ensure everything is owned by root in parcel
-sudo chown -R root:root $stagingDir/
 popd
 # validate the parcel
-java -jar $CM_EXT/validator/target/validator.jar -d $name/
+java -jar $CM_EXT/validator/target/validator.jar -d $parcelName/
 # create parcel
-tar -zcf ${name}-${OS}.parcel $name/
+tar -zcf ${parcelName}-${OS}.parcel --owner root --group root $parcelName/
 # install in http dir
-mv ${name}-${OS}.parcel $PARCEL_REPO/
+mv ${parcelName}-${OS}.parcel $PARCEL_REPO/
 # regen manifest
 $stagingDir/bin/python $CM_EXT/make_manifest/make_manifest.py $PARCEL_REPO/
